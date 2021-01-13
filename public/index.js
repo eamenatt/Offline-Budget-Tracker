34efb223429543a503d6672ea1a90ccb32a8c4c1
Eric M <eamenatt@gmail.com>
2021-01-13 14:19:43 -0500

    trying to fix serviceworker

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+    console.log('Service worker registered successfully', registration);
+  }).catch(function(err) {
+    console.log('Service worker registration failed: ', err);
+  });
+};
+
 let transactions = [];
 let myChart;
 
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/styles.css",
   "/index.js",
-  "/manifest.webmanifest",
+  "/manifest.json",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
