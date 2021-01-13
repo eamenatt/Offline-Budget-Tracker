6a1f64fc00055e73db26e4be03787750433a0f87
Eric M <eamenatt@gmail.com>
2021-01-13 13:29:49 -0500

    fixing error with the serviceworker.js files to cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/index.js",
     "/manifest.json", 
-    "/service-worker.js",
+    "/serviceworker.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
   ];
